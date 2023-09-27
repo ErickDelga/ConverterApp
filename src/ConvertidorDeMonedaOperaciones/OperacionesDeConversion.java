71aceca5147ce18695a59e35d031a195cb20c66f
ErickDelga <erick007503@gmail.com>
2023-09-26 23:28:18 -0600

    Implementacion de metodos, ya en funcion con interfasGrafica

--- src/ConvertidorDeMonedaOperaciones/OperacionesDeConversion.java
+++ src/ConvertidorDeMonedaOperaciones/OperacionesDeConversion.java
@@ -6,74 +6,50 @@ public class OperacionesDeConversion {
 	
 	public double deColonSalvadorehnoADolar(double cantidad) {
 		valorDeMonedaAComvertir = 0.11428572;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 	public double deDolarAColonSalvadorehno(double cantidad) {
 		valorDeMonedaAComvertir = 8.75;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
-	public double deDolarAPesoMexicano(double cantidad) {
-		valorDeMonedaAComvertir = 17.41;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+	public double deColonSalvadorehnoAPesoMexicano(double cantidad) {
+		valorDeMonedaAComvertir = 2.00;
+		return cantidad * valorDeMonedaAComvertir;
 	}
-	public double dePesoMexicanoADolar(double cantidad) {
-		valorDeMonedaAComvertir = 0.057;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+	public double dePesoMexicanoAColonSalvadorenho(double cantidad) {
+		valorDeMonedaAComvertir = 0.50;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 	public double deColonSalvadorehnoAEuros(double cantidad) {
 		valorDeMonedaAComvertir = 0.109;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 	public double deEurosAColonSalvadorehno(double cantidad) {
 		valorDeMonedaAComvertir = 9.259;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 	public double deColonSalvadorehnoALibrasEsterlinas(double cantidad) {
 		valorDeMonedaAComvertir = 0.094;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 	public double deLibrasEsterlinasAColonSalvadorehno(double cantidad) {
 		valorDeMonedaAComvertir = 10.652;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 	public double deColonSalvadorehnoAYenJapones(double cantidad) {
 		valorDeMonedaAComvertir = 17.03;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 	public double deYenJaponesAColonSalvadorehno(double cantidad) {
 		valorDeMonedaAComvertir = 0.059;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
-	public double deColonSalvadorehnoAWonSulCoriano(double cantidad) {
+	public double deColonSalvadorehnoAWonSurCoriano(double cantidad) {
 		valorDeMonedaAComvertir = 154.62;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
-	public double deWonSulCorianoAColonSalvadorehno(double cantidad) {
+	public double deWonSurCorianoAColonSalvadorehno(double cantidad) {
 		valorDeMonedaAComvertir = 0.0065;
-		double resultado = cantidad * valorDeMonedaAComvertir;
-		
-		return resultado;
+		return cantidad * valorDeMonedaAComvertir;
 	}
 }
--- src/InteracionConElUsuario/InterfasGrafica.java
+++ src/InteracionConElUsuario/InterfasGrafica.java
@@ -5,44 +5,47 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+
+import ConvertidorDeMonedaOperaciones.OperacionesDeConversion;
+
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.Color;
 import javax.swing.JCheckBox;
 import javax.swing.JList;
+import javax.swing.JOptionPane;
 import javax.swing.JComboBox;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
 
-public class InterfasGrafica extends JFrame {
+public class InterfasGrafica extends JFrame implements ActionListener{
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField textCantidad;
-
-	/**
-	 * Launch the application.
-	 */
-	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					InterfasGrafica frame = new InterfasGrafica();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-	}
-
-	/**
-	 * Create the frame.
-	 */
+	private JComboBox comboBoxMonedaInicial;
+	private JButton btnConvertir;
+	private double cantidad;
+	private double operacion;
+	
+	OperacionesDeConversion misOperaciones = new OperacionesDeConversion();
+	JLabel lblResultado;
+	
 	public InterfasGrafica() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		iniciarCompientes();
+		
+		setTitle("Convertidor de Monedas");
+		setResizable(false);
+		setLocationRelativeTo(null);
+	}
+
+	private void iniciarCompientes() {
+		
 		setBounds(100, 100, 670, 565);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -64,23 +67,30 @@ public class InterfasGrafica extends JFrame {
 		lblTitulo2.setBounds(0, 53, 645, 14);
 		contentPane.add(lblTitulo2);
 		
-		JComboBox comboBoxMonedaInicial = new JComboBox();
+		comboBoxMonedaInicial = new JComboBox();
 		comboBoxMonedaInicial.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
-		comboBoxMonedaInicial.setModel(new DefaultComboBoxModel(new String[] {"-->Selecione Moneda a Convertir<--", "Dolar Estadounidense", "Peso Mexicano"}));
-		comboBoxMonedaInicial.setBounds(42, 78, 235, 22);
+		comboBoxMonedaInicial.setModel(new DefaultComboBoxModel(new String[] {"-->Selecione Moneda a Convertir<--", 
+																			"Colon Salvadoreño a Dolar Estadounidense", 
+																			"Dolar Estadounidense a Colon Salvadoreño", 
+																			"Colon Salvadoreño a Peso Mexicano", 
+																			"Peso Mexicano a Colon Salvadoreño", 
+																			"Colon Salvadoreño a Euros", 
+																			"Euros a Colon Salvadoreño", 
+																			"Colon Salvadoreño a Libras Esterlinas", 
+																			"Libras Esterlinas a Colon Salvadoreño ", 
+																			"Colon Salvadoreño a Yen Japones", 
+																			"Yen Japones a Colon Salvadoreño", 
+																			"Colon Salvadoreño a Won Sur-Coriano", 
+																			"Won Sur-Coriano a Colon Salvadoreño"}));
+		comboBoxMonedaInicial.setBounds(110, 78, 435, 22);
 		contentPane.add(comboBoxMonedaInicial);
 		
-		JComboBox comboBoxMonedaFinal = new JComboBox();
-		comboBoxMonedaFinal.setModel(new DefaultComboBoxModel(new String[] {"-->Selecione Moneda a Convertir<--", "Dolar Estadounidense", "Peso Mexicano"}));
-		comboBoxMonedaFinal.setFont(new Font("Trebuchet MS", Font.BOLD, 12));
-		comboBoxMonedaFinal.setBounds(364, 79, 235, 22);
-		contentPane.add(comboBoxMonedaFinal);
-		
-		JButton btnConvertir = new JButton("Convertir");
+		btnConvertir = new JButton("Convertir");
 		btnConvertir.setBackground(new Color(128, 255, 128));
 		btnConvertir.setForeground(new Color(128, 0, 255));
 		btnConvertir.setFont(new Font("Trebuchet MS", Font.ITALIC, 22));
 		btnConvertir.setBounds(233, 197, 199, 38);
+		btnConvertir.addActionListener(this);
 		contentPane.add(btnConvertir);
 		
 		textCantidad = new JTextField();
@@ -96,10 +106,75 @@ public class InterfasGrafica extends JFrame {
 		lblCantidad.setBounds(190, 137, 146, 17);
 		contentPane.add(lblCantidad);
 		
-		JLabel lblResultado = new JLabel("AutoBorrable");
+		lblResultado = new JLabel("AutoBorrable");
 		lblResultado.setFont(new Font("Trebuchet MS", Font.BOLD | Font.ITALIC, 25));
 		lblResultado.setHorizontalAlignment(SwingConstants.CENTER);
 		lblResultado.setBounds(10, 302, 635, 108);
 		contentPane.add(lblResultado);
+		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		int tipoDeConversion = comboBoxMonedaInicial.getSelectedIndex();
+		if(btnConvertir == e.getSource() && tipoDeConversion == 1) {
+			//double cantidad = (double) Integer.parseInt(textCantidad.getText());
+			cantidad = Double.parseDouble(textCantidad.getText());
+			//System.out.println(cantidad);
+			operacion = misOperaciones.deColonSalvadorehnoADolar(cantidad);
+			
+			lblResultado.setText("La Cantidad en Dolares Estadounidenses es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 2) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deDolarAColonSalvadorehno(cantidad);
+			lblResultado.setText("La Cantidad en Colones Salvadoreños es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 3) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deColonSalvadorehnoAPesoMexicano(cantidad);
+			lblResultado.setText("La Cantidad en Pesos Mexicanos es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 4) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.dePesoMexicanoAColonSalvadorenho(cantidad);
+			lblResultado.setText("La Cantidad en Colones Salvadoreños es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 5) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deColonSalvadorehnoAEuros(cantidad);
+			lblResultado.setText("La Cantidad en Euro es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 6) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deEurosAColonSalvadorehno(cantidad);
+			lblResultado.setText("La Cantidad en Colones Salvadoreños es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 7) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deColonSalvadorehnoALibrasEsterlinas(cantidad);
+			lblResultado.setText("La Cantidad en Libras Esterlinas es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 8) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deLibrasEsterlinasAColonSalvadorehno(cantidad);
+			lblResultado.setText("La Cantidad en Colones Salvadoreños es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 9) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deColonSalvadorehnoAYenJapones(cantidad);
+			lblResultado.setText("La Cantidad en Yen Japones es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 10) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deYenJaponesAColonSalvadorehno(cantidad);
+			lblResultado.setText("La Cantidad en Colones Salvadoreños es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 11) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deColonSalvadorehnoAWonSurCoriano(cantidad);
+			lblResultado.setText("La Cantidad en Won Sur-Coriano es: "+ operacion);
+		}if(btnConvertir == e.getSource() && tipoDeConversion == 12) {
+			cantidad = Double.parseDouble(textCantidad.getText());
+			operacion = misOperaciones.deWonSurCorianoAColonSalvadorehno(cantidad);
+			lblResultado.setText("La Cantidad en Colones Salvadoreños es: "+ operacion);
+		}else if(tipoDeConversion == 0){
+			
+			lblResultado.setText("Debe Selecionar Una Forma De Convercion");
+		}
+				
+	}
+	public void asignarOperaciones (OperacionesDeConversion misOperaciones){
+		this.misOperaciones = misOperaciones;
 	}
 }
